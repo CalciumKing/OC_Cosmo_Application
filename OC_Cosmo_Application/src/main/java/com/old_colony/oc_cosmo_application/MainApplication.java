89a34d85b006e6af9a720d81c792ed1ef04726a7
CalciumKing <129754936+CalciumKing@users.noreply.github.com>
2025-04-08 14:17:54 -0400

    big changes, pretty calendar working

--- OC_Cosmo_Application/src/main/java/com/old_colony/oc_cosmo_application/MainApplication.java
+++ OC_Cosmo_Application/src/main/java/com/old_colony/oc_cosmo_application/MainApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,7 +13,8 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setTitle("Cosmetology Application | Login");
         stage.setScene(scene);
         stage.show();
     }
